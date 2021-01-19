b552a4be7229739450d30e033e58d8fd76775e51
arshess <s3th80@gmail.com>
2021-01-19 16:10:09 +0100

    beau calendar

--- partie8/tp.php
+++ partie8/tp.php
@@ -25,13 +25,11 @@ else{
     body{
         background : #4E69B3;
     }
-    .calendar{
-        min-height : 300px;
-    }
-    .alert-success{
+    .case{
+        height : 60px;
         background : #A6B0D8;
     }
-    .alert-warning{
+    .horsmois{
         background : #B56A52;
     }
     .btn{
@@ -40,7 +38,29 @@ else{
     input{
         background : #F8F8F6;
     }
-    
+    .horsmois{
+        opacity : 0.80;
+        
+    }
+
+    .calendar{
+        border-radius : 10px;
+        color : #F8F8F6;
+        
+    }
+    .calendar .row:first-child div:nth-child(1){
+        border-radius : 10px 0 0 0;
+    }
+    .calendar .row:first-child div:nth-child(7){
+        border-radius : 0 10px 0 0;
+    }
+    .calendar .row:last-child div:nth-child(1){
+        border-radius : 0 0 0 10px;
+    }
+    .calendar .row:last-child div:last-child{
+        border-radius : 0 0 10px 0;
+    }
+
     </style>
 </head>
 <body>
@@ -48,18 +68,18 @@ else{
     <div class="col-11">
         <label class="fw-bold" for="inputmonth">Mois :</label>
         <select name="inputmonth" id="inputmonth" class="form-select">
-            <option <?php if($month=="01") echo " selected ";?>value="01">janvier</option>
-            <option <?php if($month=="02") echo " selected ";?>value="02">fevrier</option>
-            <option <?php if($month=="03") echo " selected ";?>value="03">mars</option>
-            <option <?php if($month=="04") echo " selected ";?>value="04">avril</option>
-            <option <?php if($month=="05") echo " selected ";?>value="05">mai</option>
-            <option <?php if($month=="06") echo " selected ";?>value="06">juin</option>
-            <option <?php if($month=="07") echo " selected ";?>value="07">juillet</option>
-            <option <?php if($month=="08") echo " selected ";?>value="08">aout</option>
-            <option <?php if($month=="09") echo " selected ";?>value="09">septembre</option>
-            <option <?php if($month=="10") echo " selected ";?>value="10">octobre</option>
-            <option <?php if($month=="11") echo " selected ";?>value="11">novembre</option>
-            <option <?php if($month=="12") echo " selected ";?>value="12">décembre</option>       
+            <option <?php if($month=="01") echo " selected ";?>value="01">Janvier</option>
+            <option <?php if($month=="02") echo " selected ";?>value="02">Fevrier</option>
+            <option <?php if($month=="03") echo " selected ";?>value="03">Mars</option>
+            <option <?php if($month=="04") echo " selected ";?>value="04">Avril</option>
+            <option <?php if($month=="05") echo " selected ";?>value="05">Mai</option>
+            <option <?php if($month=="06") echo " selected ";?>value="06">Juin</option>
+            <option <?php if($month=="07") echo " selected ";?>value="07">Juillet</option>
+            <option <?php if($month=="08") echo " selected ";?>value="08">Août</option>
+            <option <?php if($month=="09") echo " selected ";?>value="09">Septembre</option>
+            <option <?php if($month=="10") echo " selected ";?>value="10">Octobre</option>
+            <option <?php if($month=="11") echo " selected ";?>value="11">Novembre</option>
+            <option <?php if($month=="12") echo " selected ";?>value="12">Décembre</option>       
         </select>
         <label class="fw-bold" for="inputyear">Année :</label>
         <select name="inputyear" id="inputyear" class="form-select">
@@ -77,28 +97,28 @@ else{
     </div>
     <div class="col-1 ">  
     <br/>
-        <input class="truc h-75 my-1 btn fw-bold text-white" name="submit" type="submit" value="Generate">
+        <input class="truc h-75 my-1 btn fw-bold  text-white" name="submit" type="submit" value="Generate">
     <br/>
     </div>    
     </form>
     <!-- calendar -->
-    <div class="container">
+    <div class="container row col-8 offset-2">
 
     <?php
     if(isset($_POST['submit'])){?>
 
     <h1 class="text-center text-white fw-bold my-3">Calendrier de <?=tradDate($mois) . " " . $year?></h1>
 
-    <div class="row text-white fw-bold">
-        <div class="col">lundi</div>
-        <div class="col">mardi</div>
-        <div class="col">mercredi</div>
-        <div class="col">jeudi</div>
-        <div class="col">vendredi</div>
-        <div class="col">samedi</div>
-        <div class="col">dimanche</div>
+    <div class="row text-white text-end fw-bold">
+        <div class="col">Lundi</div>
+        <div class="col">Mardi</div>
+        <div class="col">Mercredi</div>
+        <div class="col">Jeudi</div>
+        <div class="col">Vendredi</div>
+        <div class="col">Samedi</div>
+        <div class="col">Dimanche</div>
     </div>
-    <div class="row col-12 shadow calendar">
+    <div class="row-fluid col-12 shadow calendar fw-bold text-end">
     <?php
     //pour chaque jour du mois, on affiche une case
     //On utilise une row pour chaque semaine (on l'ouvre le lundi et on la ferme le dimanche).
@@ -120,11 +140,11 @@ else{
             for ($fk=0; $fk < $compteuralacon ; $fk++) { 
                 $day = date("d", mktime(0,0,0,$month,$j,$year));
                 $j++;
-                echo "<div class='col alert-warning text-right'>" . $day . "</div>";
+                echo "<div class='col case horsmois '>" . $day . "</div>";
             }
         }
-        //cases du mois à la con
-        echo "<div class='col alert-success'>".$i."</div>";
+        //cases du mois
+        echo "<div class='col case'>".$i."</div>";
         //on la ferme le dimanche
         if(date("l", mktime(0,0,0,$month,$i,$year))== "Sunday"){
             echo"</div>";
@@ -137,7 +157,7 @@ else{
             while(date("l", mktime(0,0,0,$month,$daycount,$year))!= "Sunday"){
                 $daycount++;
                 $day = date("d", mktime(0,0,0,$month,$daycount,$year));
-                echo "<div class='col alert-warning text-right'>" . $day . "</div>";
+                echo "<div class='col case horsmois'>" . $day . "</div>";
             }
             break;
         }
